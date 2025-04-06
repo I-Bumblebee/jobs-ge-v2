5269abbe1421264105fade9e3b0d456d9de0a7fa
luka trapaidze <trapaidze.luka@kiu.edu.ge>
2025-04-06 22:12:57 +0400

    +

--- entrypoints/jobs.content/composables/usePageRouter.ts
+++ entrypoints/jobs.content/composables/usePageRouter.ts
@@ -1,17 +1,17 @@
 import {useRouter} from "vue-router";
 import {usePagePropsStore} from "@/entrypoints/jobs.content/stores/pagePropsStore";
-import {TEST} from "@/entrypoints/jobs.content/constants/pageNames";
+import {PageName, TEST} from "@/entrypoints/jobs.content/constants/pageNames";
 import jobListTableParser from "@/entrypoints/jobs.content/parsers/jobListTableParser"
 
 const routeConfigs = [
     {
         externalPath: '/',
         internalName: TEST,
-        parser: jobListTableParser
+        parser: (doc: Document) => ({jobList: jobListTableParser(doc)})
     },
 ];
 
-function determineRouteNameFromURL(url: string): string {
+function determineRouteNameFromURL(url: string): PageName {
     return TEST;
 }
 
@@ -20,7 +20,7 @@ export function usePageRouter() {
     const viewData = ref({})
     const pagePropsStore = usePagePropsStore();
 
-    function parseAndRedirect(htmlContent: string, redirect: boolean = true, targetRouteName?: string) {
+    function parseAndRedirect(htmlContent: string, redirect: boolean = true, targetRouteName?: PageName) {
         const routeName = targetRouteName || determineRouteNameFromURL(window.location.href)
         const parserConfig = routeConfigs.find(config => config.internalName === routeName)
 
@@ -36,7 +36,7 @@ export function usePageRouter() {
 
         if (redirect)
         {
-            pagePropsStore.setPageProps(TEST, {jobList: value})
+            pagePropsStore.setPageProps(routeName, value)
             void router.push({
                 name: routeName,
             })
