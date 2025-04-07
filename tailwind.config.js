03a35877f41392b0b2e4f32f38e68d1e9b342270
luka trapaidze <trapaidze.luka@kiu.edu.ge>
2025-04-07 23:51:37 +0400

    downgrade tailwindcss to stable v3

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./entrypoints/jobs.content/**/*.{vue,js,ts,html}'],
+    theme: {
+        extend: { },
+    },
+    plugins: [],
+}
--- wxt.config.ts
+++ wxt.config.ts
@@ -1,12 +1,8 @@
 import { defineConfig } from 'wxt';
-import tailwindcss from "@tailwindcss/vite";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ['@wxt-dev/module-vue'],
-  vite: () => ({
-    plugins: [tailwindcss()],
-  }),
   manifest: {
     web_accessible_resources: [
       {
